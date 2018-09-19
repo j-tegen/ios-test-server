0c7e7af97217d369e5e757225ff289e0209ef800
Jonatan Tegen <jonatan@Jonatans-MacBook-Pro-2.local>
2018-09-19 12:42:13 +0200

    Remove list routes in manage.py

--- manage.py
+++ manage.py
@@ -71,38 +71,6 @@ def reset():
     drop_db()
     create_db()
 
-
-@manager.command
-def list_routes():
-    from urllib.parse import unquote
-    routes = []
-
-    hidden_endpoints = ['static']
-    relevant_methods = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
-    for rule in app.url_map.iter_rules():
-        if rule.endpoint in hidden_endpoints:
-            continue
-        options = {}
-        for arg in rule.arguments:
-            options[arg] = "<{0}>".format(arg)
-
-        methods = ','.join(
-            [method for method in rule.methods if method in relevant_methods])
-        url = url_for(rule.endpoint, **options)
-        url = url.replace('/api/v1/api/v1', '/api/v1')
-
-        if hasattr(app.view_functions[rule.endpoint], 'import_name'):
-            import_name = app.view_functions[rule.endpoint].import_name
-            obj = import_string(import_name)
-            line = unquote("{:50s} {:20s} {:40s} {}".format(rule.endpoint, methods, url, obj.__doc__))
-            routes.append(line)
-        else:
-            line = unquote("{:50s} {:20s} {:40s} {}".format(rule.endpoint, methods, url, app.view_functions[rule.endpoint].__doc__))
-            routes.append(line)
-
-    for line in sorted(routes):
-        print(line)
-
 server = Server(host=settings.host_ip, port=settings.port)
 manager.add_command("runserver", server)
 
