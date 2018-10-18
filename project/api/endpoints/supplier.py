33582610a8a6b8b88e20bcac66127694ae905d10
Jonatan Tegen <jonatan.tegen@gmail.com>
2018-10-18 16:28:57 +0200

    [supplier] - Fix bug with appending reclamation to supplier

--- project/api/endpoints/supplier.py
+++ project/api/endpoints/supplier.py
@@ -176,7 +176,7 @@ def create_reclamation(args, id):
     """Private"""
     supplier = Supplier.query.get(id)
     reclamation = Reclamation(**args)
-    supplier.add(reclamation)
+    supplier.append(reclamation)
     db.session.add(supplier)
     db.session.commit()
     return make_response(
